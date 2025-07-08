c063bc8a0c01ab70357f5baebd5a72b891995dbf
Marvin Klerx <marvinklerx20@gmail.com>
2025-07-09 00:11:18 +0200

    added new tests and checks

--- atest/test_cli.py
+++ atest/test_cli.py
@@ -32,3 +32,27 @@ def test_cli_cmd_verbose():
     assert "output.html" in result.output
     assert "Saving" in result.output
     assert os.path.exists(output)
+
+def test_html_text_content():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    robot = os.path.join(current_dir, "test_cli.robot")
+    output = os.path.join(current_dir, "output.html")
+    runner = CliRunner()
+    result = runner.invoke(main, [robot, output, "-m", "Test=UnitTesting"])
+    assert result.exit_code == 0
+    assert "Generated" in result.output
+    assert "output.html" in result.output
+    assert os.path.exists(output)
+    with open(output, "r", encoding="utf-8") as f:
+        html_content = f.read()
+    assert "UnitTesting" in html_content
+    assert "Marvin Klerx" in html_content
+    assert "Log Message" in html_content
+    assert "test_cli.robot" in html_content
+    assert "Test Cli" in html_content
+    assert "Basic Console Logging - Suite Doc" in html_content
+    assert "Basic Console Logging - Test Doc" in html_content
+    assert "Global-Tag, RobotTestDoc" in html_content
+    assert "RobotFramework Test Documentation Generator!" in html_content
+    assert "Robot Framework - Test Documentation" in html_content
+
--- atest/test_cli.robot
+++ atest/test_cli.robot
@@ -7,6 +7,6 @@ Test Tags    Global-Tag
 
 *** Test Cases ***
 Log Message
-    [Documentation]    Basic Console Logging
+    [Documentation]    Basic Console Logging - Test Doc
     [Tags]    RobotTestDoc
     Log    RobotFramework Test Documentation Generator!
